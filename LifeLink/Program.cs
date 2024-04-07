3b05451470bb5f54430277e5fd66e57acc1b3559
Mert Gürer <mert.gurer@hotmail.com.tr>
2024-04-07 20:00:54 +0300

    setup

--- LifeLink/Program.cs
+++ LifeLink/Program.cs
@@ -23,7 +23,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
